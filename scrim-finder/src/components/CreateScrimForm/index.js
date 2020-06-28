3317c6f4daa4b7901f6a0b541623546f27ebafb3
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-06-28 01:24:35 -0400

    added error checking for creating scrim data

--- scrim-finder/src/components/CreateScrimForm/index.js
+++ scrim-finder/src/components/CreateScrimForm/index.js
@@ -19,8 +19,77 @@ const initialFormData = {
   region: "",
 };
 
+const initialErrorData = {
+  teamName: "Must enter a team name",
+  date: "Cannot make a scrim for a previous date",
+  startTime: "Cannot create a start time at selected time",
+  endTime: "End time must be after start time",
+  elo: "Must enter an elo",
+  region: "Must enter a region",
+};
+
 const CreateScrimForm = (props) => {
   const [formData, updateFormData] = useState(initialFormData);
+  const [errorData, updateErrorData] = useState(initialFormData);
+
+  let d = new Date();
+
+  const verifyFormData = () => {
+    let error = { ...initialErrorData };
+    let numOfErrors = 6;
+
+    let year = d.getFullYear();
+    let month = (d.getMonth() + 1).toString().padStart(2, "0");
+    let date = d.getDate();
+
+    let fullDate = `${year}-${month}-${date}`;
+
+    let hour = d.getHours().toString().padStart(2, "0");
+    let minute = d.getMinutes();
+
+    let curTime = `${hour}:${minute}`;
+
+    if (formData.teamName !== "") {
+      error.teamName = "";
+      numOfErrors -= 1;
+    }
+
+    if (formData.date !== "" && formData.date >= fullDate) {
+      error.date = "";
+      numOfErrors -= 1;
+    }
+
+    if (
+      formData.startTime !== "" &&
+      (formData.date !== fullDate ||
+        formData.startTime.localeCompare(curTime)) !== -1
+    ) {
+      error.startTime = "";
+      numOfErrors -= 1;
+    }
+
+    if (
+      formData.endTime !== "" &&
+      formData.startTime.localeCompare(formData.endTime) === -1
+    ) {
+      error.endTime = "";
+      numOfErrors -= 1;
+    }
+
+    if (formData.elo !== "") {
+      error.elo = "";
+      numOfErrors -= 1;
+    }
+
+    if (formData.region !== "") {
+      error.region = "";
+      numOfErrors -= 1;
+    }
+
+    updateErrorData(error);
+
+    return numOfErrors === 0;
+  };
 
   const handleChange = (event) => {
     updateFormData({
@@ -31,28 +100,30 @@ const CreateScrimForm = (props) => {
   };
 
   const handleSubmit = (event) => {
-    let request = `http://localhost:5000/scrims`;
-    console.log(formData);
+    if (verifyFormData()) {
+      let request = `http://localhost:5000/scrims`;
+      console.log(formData);
 
-    fetch(request, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        mode: "cors",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => {
-        return response.json();
+      fetch(request, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          mode: "cors",
+        },
+        body: JSON.stringify(formData),
       })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Server Timeout");
-      });
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Server Timeout");
+        });
+    }
   };
 
   return (
@@ -69,6 +140,7 @@ const CreateScrimForm = (props) => {
           name="teamName"
           onChange={handleChange}
         />
+        <span className="error">{errorData.teamName}</span>
         <label className="label" for="game">
           Game:
         </label>
@@ -90,8 +162,10 @@ const CreateScrimForm = (props) => {
           type="date"
           id="date"
           name="date"
+          min={`${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`}
           onChange={handleChange}
         />
+        <span className="error">{errorData.date}</span>
         <label className="label" for="startTime">
           Start Time:
         </label>
@@ -102,6 +176,7 @@ const CreateScrimForm = (props) => {
           name="startTime"
           onChange={handleChange}
         />
+        <span className="error">{errorData.startTime}</span>
         <label className="label" for="endTime">
           End Time:
         </label>
@@ -112,6 +187,7 @@ const CreateScrimForm = (props) => {
           name="endTime"
           onChange={handleChange}
         />
+        <span className="error">{errorData.endTime}</span>
         <label className="label" for="elo">
           Elo:
         </label>
@@ -122,6 +198,7 @@ const CreateScrimForm = (props) => {
           name="elo"
           onChange={handleChange}
         />
+        <span className="error">{errorData.elo}</span>
         <label className="label" for="region">
           Region:
         </label>
@@ -132,6 +209,7 @@ const CreateScrimForm = (props) => {
           name="region"
           onChange={handleChange}
         />
+        <span className="error">{errorData.region}</span>
         <button className="submit" onClick={handleSubmit}>
           Submit
         </button>
