e677c63c2a18f7ec80ff335471a9af3ccbbc721f
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-07-25 22:29:40 -0400

    added predetermined elo ratings

--- scrim-finder/src/components/CreateScrimForm/index.js
+++ scrim-finder/src/components/CreateScrimForm/index.js
@@ -19,6 +19,76 @@ const initialFormData = {
   region: "",
 };
 
+const OverwatchElo = [
+  "Bronze",
+  "Silver",
+  "Gold",
+  "Platinum",
+  "Diamond",
+  "Masters",
+  "Grandmaster",
+];
+
+const ValorantElo = [
+  "Iron 1",
+  "Iron 2",
+  "Iron 3",
+  "Bronze 1",
+  "Bronze 2",
+  "Bronze 3",
+  "Silver 1",
+  "Silver 2",
+  "Silver 3",
+  "Gold 1",
+  "Gold 2",
+  "Gold 3",
+  "Platinum 1",
+  "Platinum 2",
+  "Platinum 3",
+  "Diamond 1",
+  "Diamond 2",
+  "Diamond 3",
+  "Immortal 1",
+  "Immortal 2",
+  "Immortal 3",
+  "Radiant",
+];
+
+const CSGOElo = [
+  "Silver I",
+  "Silver II",
+  "Silver III",
+  "Silver IV",
+  "Silver Elite",
+  "Silver Elite Master",
+  "Gold Nova I",
+  "Gold Nova II",
+  "Gold Nova III",
+  "Gold Nova Master",
+  "Master Guardian I",
+  "Master Guardian II",
+  "Master Guardian Elite",
+  "Distinguished Master Guardian",
+  "Legendary Eagle",
+  "Legendary Eagle Master",
+  "Supreme Master First Class",
+  "The Global Elite",
+];
+
+const LeagueOfLegendsElo = [
+  "Iron",
+  "Bronze",
+  "Silver",
+  "Gold",
+  "Platinum",
+  "Diamond",
+  "Master",
+  "Grand Master",
+  "Challenger",
+];
+
+const listOfElo = [OverwatchElo, ValorantElo, CSGOElo, LeagueOfLegendsElo];
+
 const initialErrorData = {
   teamName: "Must enter a team name",
   date: "Cannot make a scrim for a previous date",
@@ -126,6 +196,14 @@ const CreateScrimForm = (props) => {
     }
   };
 
+  const getEloRankings = () => {
+    for (let i = 0; i < listOfGames.length; i++) {
+      if (listOfGames[i] === formData.game) {
+        return listOfElo[i];
+      }
+    }
+  };
+
   return (
     <>
       <div class="create-scrim-form">
@@ -191,13 +269,16 @@ const CreateScrimForm = (props) => {
         <label className="label" for="elo">
           Elo:
         </label>
-        <input
+        <select
           className="create-input"
-          type="text"
           id="elo"
           name="elo"
           onChange={handleChange}
-        />
+        >
+          {getEloRankings().map((i) => (
+            <option>{i}</option>
+          ))}
+        </select>
         <span className="error">{errorData.elo}</span>
         <label className="label" for="region">
           Region:
--- scrim-finder/src/components/FindScrimForm/index.js
+++ scrim-finder/src/components/FindScrimForm/index.js
@@ -10,6 +10,76 @@ const listOfGames = [
   "Dota 2",
 ];
 
+const OverwatchElo = [
+  "Bronze",
+  "Silver",
+  "Gold",
+  "Platinum",
+  "Diamond",
+  "Masters",
+  "Grandmaster",
+];
+
+const ValorantElo = [
+  "Iron 1",
+  "Iron 2",
+  "Iron 3",
+  "Bronze 1",
+  "Bronze 2",
+  "Bronze 3",
+  "Silver 1",
+  "Silver 2",
+  "Silver 3",
+  "Gold 1",
+  "Gold 2",
+  "Gold 3",
+  "Platinum 1",
+  "Platinum 2",
+  "Platinum 3",
+  "Diamond 1",
+  "Diamond 2",
+  "Diamond 3",
+  "Immortal 1",
+  "Immortal 2",
+  "Immortal 3",
+  "Radiant",
+];
+
+const CSGOElo = [
+  "Silver I",
+  "Silver II",
+  "Silver III",
+  "Silver IV",
+  "Silver Elite",
+  "Silver Elite Master",
+  "Gold Nova I",
+  "Gold Nova II",
+  "Gold Nova III",
+  "Gold Nova Master",
+  "Master Guardian I",
+  "Master Guardian II",
+  "Master Guardian Elite",
+  "Distinguished Master Guardian",
+  "Legendary Eagle",
+  "Legendary Eagle Master",
+  "Supreme Master First Class",
+  "The Global Elite",
+];
+
+const LeagueOfLegendsElo = [
+  "Iron",
+  "Bronze",
+  "Silver",
+  "Gold",
+  "Platinum",
+  "Diamond",
+  "Master",
+  "Grand Master",
+  "Challenger",
+];
+
+const listOfElo = [OverwatchElo, ValorantElo, CSGOElo, LeagueOfLegendsElo];
+
 const initialFormData = {
   game: "",
   date: "",
@@ -37,6 +107,17 @@ const FindScrimForm = (props) => {
 
   const handleSubmit = (event) => {
     props.callback(formData);
+  };
+
+  const getEloRankings = () => {
+    if (formData.game === "") {
+      return [""];
+    }
+    for (let i = 0; i < listOfGames.length; i++) {
+      if (listOfGames[i + 1] === formData.game) {
+        return listOfElo[i];
+      }
+    }
   };
 
   return (
@@ -89,13 +170,16 @@ const FindScrimForm = (props) => {
         <label className="label" for="elo">
           Elo:
         </label>
-        <input
+        <select
           className="find-input"
-          type="text"
           id="elo"
           name="elo"
           onChange={handleChange}
-        />
+        >
+          {getEloRankings().map((i) => (
+            <option>{i}</option>
+          ))}
+        </select>
         <label className="label" for="region">
           Region:
         </label>
